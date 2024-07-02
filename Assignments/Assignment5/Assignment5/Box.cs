70a7b04a3015855317e28a2a7ab453bbf37a727f
Pooja Kulkarni <poojakul@infinite.com>
2024-07-02 09:59:09 +0530

    assignment5

---
+++ Assignments/Assignment5/Assignment5/Box.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+     public class Box
+    {
+       
+        public double Length { get; set; }
+        public double Breadth { get; set; }
+
+        public Box(double length, double breadth)
+        {
+            Length = length;
+            Breadth = breadth;
+        }
+
+        public Box Add(Box otherBox)
+        {
+            double newLength = Length + otherBox.Length;
+            double newBreadth = Breadth + otherBox.Breadth;
+            return new Box(newLength, newBreadth);
+        }
+    }
+
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Box box1 = new Box(10, 3);
+            Box box2 = new Box(2, 40);
+
+            Box resultBox = box1.Add(box2);
+
+            Console.WriteLine($"Box 1: Length = {box1.Length}, Breadth = {box1.Breadth}");
+            Console.WriteLine($"Box 2: Length = {box2.Length}, Breadth = {box2.Breadth}");
+            Console.WriteLine($"Result Box: Length = {resultBox.Length}, Breadth = {resultBox.Breadth}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignments/Assignment5/Assignment5/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+
+    public class Books
+    {
+        public string BookName { get; set; }
+        public string AuthorName { get; set; }
+
+        public Books(string bookName, string authorName)
+        {
+            BookName = bookName;
+            AuthorName = authorName;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Book Name: {BookName}, Author Name: {AuthorName}");
+            Console.ReadLine();
+        }
+    }
+
+    class BookShelf
+    {
+        private Books[] books = new Books[6];
+
+        public Books this[int index]
+        {
+            get { return books[index]; }
+            set { books[index] = value; }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            // Create instances of Books
+            Books book1 = new Books("Book1", "Author1");
+            Books book2 = new Books("Book2", "Author2");
+            Books book3 = new Books("Book3", "Author3");
+            Books book4 = new Books("Book4", "Author4");
+            Books book5 = new Books("Book5", "Author5");
+            Books book6 = new Books("Book6", "Author6");
+            // Create an instance of BookShelf
+            BookShelf bookshelf = new BookShelf();
+
+            // Assign values to the books using the indexer
+            bookshelf[0] = book1;
+            bookshelf[1] = book2;
+            bookshelf[2] = book3;
+            bookshelf[3] = book4;
+            bookshelf[4] = book5;
+            bookshelf[5] = book6;
+
+            // Display the details of the books
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.Write($"Book {i + 1}: ");
+                if (bookshelf[i] != null)
+                    bookshelf[i].Display();
+                else
+                    Console.WriteLine("Empty");
+                     Console.ReadLine();
+            }
+        }
+    }
+}
+
+
+
+
+
+    
