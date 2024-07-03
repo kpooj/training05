d7fe6cb664c0cf57cd2a2d72e19487ae73edc12b
Pooja Kulkarni <poojakul@infinite.com>
2024-07-03 13:06:26 +0530

    cc3

---
+++ Assessment/CC3/CC3/BoxEx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC3
+{  
+       class Box
+        {
+            public double Length { get; set; }
+            public double Breadth { get; set; }
+
+            public Box(double length, double breadth)
+            {
+                Length = length ;
+                Breadth = breadth;
+
+             }
+
+        public Box Add(Box otherBox)
+            {
+                double newLength = Length + otherBox.Length;
+                double newBreadth = Breadth + otherBox.Breadth;
+                 //double newLength1 = Length + otBreadth;
+                return new Box(newLength, newBreadth);
+            }
+
+            public void DisplayDetails()
+            {
+                Console.WriteLine($"Box details: Length = {Length}, Breadth = {Breadth}");
+            }
+        }
+
+        class Test
+        {
+            static void Main(string[] args)
+            {
+                Box box1 = new Box(50, 3);
+                Box box2 = new Box(2, 40);
+
+                Box box3 = box1.Add(box2);
+                box3.DisplayDetails();
+                Console.ReadLine();
+            }
+        }
+    }
+
+
---
+++ Assessment/CC3/CC3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC3
+{
+    public class Cricket
+    {
+        public void Pointscalculation(int  no_of_matches)
+        {
+            int sum = 0;
+
+            for (int i = 1; i <= no_of_matches; i++)
+            {
+                Console.Write($"Enter the score for match of IPL {i}: ");
+                int score = Convert.ToInt32(Console.ReadLine());
+                sum += score;
+            }
+
+            double average = (double)sum / no_of_matches;
+
+            Console.WriteLine($"\nThe sum of scores for  all matches {no_of_matches} matches is: {sum}");
+            Console.WriteLine($"The average score of all matches is: {average:F3}"); // Format average to three decimal places
+        }
+
+        public static void Main(string[] args)
+        {
+            Cricket cricket = new Cricket();
+
+            Console.Write("Enter the number of matches played in the IPL : ");
+            int no_of_matches = Convert.ToInt32(Console.ReadLine());
+
+            cricket.Pointscalculation(no_of_matches);
+            Console.ReadLine();
+        }
+    }
+}
+
+//Pointscalculation(int no_of_matches)
